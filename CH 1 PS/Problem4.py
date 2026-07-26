d974364c0511b915785976aab55818002d5718e9
Ahmed-Jawad-net <ahmedwork2811@gmail.com>
2026-07-26 18:46:09 +0500

    updated

--- CH 1 PS/Problem4.py
+++ CH 1 PS/Problem4.py
@@ -1,9 +1,9 @@
 import os
 
-#select the directory who you want to list
-directory_path= '/home/jawad-ahmed/Downloads'
+# select the directory who you want to list
+directory_path = "/home/jawad-ahmed"
 
-#use the os model to list the directory content
+# use the os model to list the directory content
 content = os.listdir(directory_path)
 
 
