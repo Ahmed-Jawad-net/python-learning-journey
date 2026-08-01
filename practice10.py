75207732b13f8a393955f215c26c36b7aaac30af
Ahmed-Jawad-net <ahmedwork2811@gmail.com>
2026-08-01 07:53:52 +0500

    CH 1 Practice

---
+++ practice10.py
@@ -0,0 +1,4 @@
+#Future year calculator
+current_year = int(input("Enter the current year "))
+future_year = current_year + 5
+print(f"Future Year is: {future_year}")
---
+++ practice2.py
@@ -0,0 +1,4 @@
+age = input("Enter your age: ")
+age =int(age)  # Convert the input string to an integer
+age += 5
+print(f"Your age after 5 years will be: {age}")
